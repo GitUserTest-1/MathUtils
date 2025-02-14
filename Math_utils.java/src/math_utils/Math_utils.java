0872187286a721e4a2b9a4a358ccf35e0f960967
Alex-SM98 <Simbana.mobru23@zaragoza.salesianos.edu>
2025-02-14 17:21:08 +0100

    Agregar función divide(), versión beta

--- Math_utils.java/src/math_utils/Math_utils.java
+++ Math_utils.java/src/math_utils/Math_utils.java
@@ -3,11 +3,11 @@ package math_utils;
 public class Math_utils {
 
 	public int add(int... numbers) {
-		
+
 		int sum = 0;
-		
+
 		for (int num : numbers) {
-			
+
 			sum += num;
 		}
 		return sum;
@@ -20,6 +20,15 @@ public class Math_utils {
 	public int multiply(int a, int b) {
 
 		return a * b;
+	}
+
+	public int divide(int a, int b) {
+		
+		if (b == 0) {
+			
+			throw new ArithmeticException("No se puede dividir por cero");
+		}
+		return a / b;
 	}
 
 }
