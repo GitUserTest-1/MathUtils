c1ac28d8e60ea7bc3b4f34f92baf85171786e964
Alex-SM98 <Simbana.mobru23@zaragoza.salesianos.edu>
2025-02-14 17:15:23 +0100

    Cambio incompatible: modificar add() para aceptar múltiples argumentos

--- Math_utils.java/src/math_utils/Math_utils.java
+++ Math_utils.java/src/math_utils/Math_utils.java
@@ -2,8 +2,15 @@ package math_utils;
 
 public class Math_utils {
 
-	public int sumar(int a, int b) {
-		return a + b;
+	public int add(int... numbers) {
+		
+		int sum = 0;
+		
+		for (int num : numbers) {
+			
+			sum += num;
+		}
+		return sum;
 	}
 
 	public int subtract(int a, int b) {
